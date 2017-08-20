5a4ecd7d1ad7ab2c8ff40841bd7d2f7f1042b0b9
Nikolay Bilev <bilevn@gmail.com>
2017-08-20 23:18:42 +0300

    solutions mock

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,27 @@ $(document).ready(function(){
 
 // Scrol down
 $('.scroll-down, #nav-solutions').click (function() {
-  $('html, body').animate({scrollTop: $('section.solutions').offset().top }, 'slow');
-  return false;
+	$('html, body').animate({scrollTop: $('section.about-solutions').offset().top }, 'slow');
+	return false;
+});
+
+
+
+$(document).ready(function() { 
+	(function ($) { 
+		$('.tab ul.tabs').addClass('active').find('> li:eq(0)').addClass('current');
+		
+		$('.tab ul.tabs li a').click(function (g) { 
+			var tab = $(this).closest('.tab'), 
+				index = $(this).closest('li').index();
+			
+			tab.find('ul.tabs > li').removeClass('current');
+			$(this).closest('li').addClass('current');
+			
+			tab.find('.tab_content').find('div.tabs_item').not('div.tabs_item:eq(' + index + ')').slideUp();
+			tab.find('.tab_content').find('div.tabs_item:eq(' + index + ')').slideDown();
+			
+			g.preventDefault();
+		} );
+	})(jQuery);
 });
